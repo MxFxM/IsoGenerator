68a8bb99e9ecd2a6f87d1805eb07814b09b43979
MxFxM <mxfxmmueller@gmail.com>
2020-12-05 19:45:43 +0100

    option to view input data in 3d

--- isogenerator.py
+++ isogenerator.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 # SETTINGS AND OPTIONS
 #
 INPUT_FILENAME = "testfile"
-OPTION_PRINT_DATA_INPUT = True
-OPTION_PLOT_INPUT_DIAGRAM = True
+OPTION_PRINT_DATA_INPUT = False
+OPTION_PLOT_INPUT_DIAGRAM = False
+OPTION_PLOT_3D_INPUT_DIAGRAM = True
 
 #
 # CODE
@@ -26,4 +27,19 @@ if OPTION_PLOT_INPUT_DIAGRAM:
     plt.plot(input_data["green_x"], 150-input_data["green_y"], color="green", label="green")
     plt.plot(input_data["pink_x"], 150-input_data["pink_y"], color="pink", label="pink")
     plt.legend(loc="best")
+    plt.show()
+
+# plot input data on a 3d scatter plot
+if OPTION_PLOT_3D_INPUT_DIAGRAM:
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(input_data["black_x"], 150-input_data["black_y"], input_data["black_z"], color="black", marker='o')
+    ax.scatter(input_data["blue_x"], 150-input_data["blue_y"], input_data["blue_z"], color="blue", marker='o')
+    ax.scatter(input_data["grey_x"], 150-input_data["grey_y"], input_data["grey_z"], color="grey", marker='o')
+    ax.scatter(input_data["red_x"], 150-input_data["red_y"], input_data["red_z"], color="red", marker='o')
+    ax.scatter(input_data["green_x"], 150-input_data["green_y"], input_data["green_z"], color="green", marker='o')
+    ax.scatter(input_data["pink_x"], 150-input_data["pink_y"], input_data["pink_z"], color="pink", marker='o')
+    ax.set_xlabel("X-Axis")
+    ax.set_ylabel("Y-Axis")
+    ax.set_zlabel("Height")
     plt.show()
